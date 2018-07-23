24d51b7111f1043fd74410408a4d5ebbca1f6212
Jiayuee <zhangjiayuelily@gmail.com>
2018-07-23 17:50:56 +0800

    subset for selected diseases

---
+++ etl2.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+import numpy as np
+import pickle
+from pandas import DataFrame
+
+#### import dataframe
+ip = "data.pkl"
+op = pd.HDFStore('Proportions2.h5')
+df = pd.read_pickle(ip)
+df = df.sort_values(['identifier','year'])
+df = df.set_index(np.arange(len(df)))
+
+## select subset of dataframe
+d_list = ['dCD_AtrFib','dCD_CHD','dCD_DM','dCD_LPD','dCD_HeartFail','dCD_Hyp',
+            'dCD_PVD', 'dCD_Renal','dCD_Rheumatoid', 'dCD_Stroke',
+            'dCD_SuspectedStroke','dCCI_AMI','dCCI_CVD','dCCI_CHF',
+            'dCCI_DMCompl','dCCI_DMNoCompl', 'dCCI_PVD','dCCI_Renal',
+            'dCCI_Rheuma']
+cols = ['identifier','year','dDeath'] + d_list
+df = df[cols]
+sub_df = df.drop_duplicates(subset=['identifier'], keep = 'last')
+
+op = pd.HDFStore('edited_data2.h5')
+op['latest_disease_record'] = sub_df
+
+op['total_disease_record'] = df
